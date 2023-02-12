6e8ef7d692abc678781b37326e6a72763f381cc2
Juan Cruz Gonzalez <juancgonzalez0901@gmail.com>
2023-02-12 18:02:35 -0300

    Ejercicio_2 JS OpenBootcamp

---
+++ Curso-Javascript/mi-primer-proyecto-de-node/Ejercicio_2.js
@@ -0,0 +1,7 @@
+const nombre = "Juan Cruz";
+const edad = 21;
+const desarrollador = true;
+const fecha_nacimiento = new Date(2001,8,25)
+const libro = ["Harry Potter", "J.K.Rowling", new Date(1997,5,26), "https://www.amazon.com/Harry-Potter-Sorcerers-Stone-Rowling/dp/0590353403/ref=sr_1_1?adgrpid=79841067965&gclid=CjwKCAiAuaKfBhBtEiwAht6H76S2u2QewHk4bjbUAfKZz-U1nnh3WsNioGwX69cwb6MjaqtcJqtJ_RoCMlYQAvD_BwE&hvadid=585478886102&hvdev=c&hvlocphy=1000092&hvnetw=g&hvqmt=b&hvrand=10698974708298438803&hvtargid=kwd-299586061139&hydadcr=19457_13388740&keywords=harry+potter+book+1+hardcover&qid=1676235387&sr=8-1"];
+const lista_juan_cruz = [nombre,edad,desarrollador,fecha_nacimiento,libro]
+console.log(lista_juan_cruz);
