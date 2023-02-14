ac1d6d1e06b2ce2b3e6059253be0370dc3c65eec
Juan Cruz Gonzalez <juancgonzalez0901@gmail.com>
2023-02-14 14:39:07 -0300

    Ejercicio 4 de JS en OpenBootcamp

---
+++ Curso-Javascript/Ejercicio_4/Ejercicio_4.js
@@ -0,0 +1,10 @@
+nombre = "Juan Cruz"
+apellido = "Gonzalez"
+estudiante = nombre + " " + apellido
+estudianteMayus = estudiante.toUpperCase()
+estudianteMinus = estudiante.toLowerCase()
+let longitud_estudiante = estudiante.length
+primera_letra_nombre = nombre[0]
+ultima_letra_apellido = apellido[7]
+sin_espacios_estudiante = estudiante.trim()
+nombre_en_estudiante = estudiante.includes("Juan")
